a7074530e251d3364123a08614a678cf46f632df
Zaber Ahmed <zaber0305@gmail.com>
2023-12-10 13:52:01 +0600

    Modify: restructure dashboard profile section

--- app/dashboard/profile-section.tsx
+++ app/dashboard/profile-section.tsx
@@ -19,11 +19,28 @@ const Profile = () => {
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>ZA</AvatarFallback>
           </Avatar>
-          <p className={`text-lg ${pacifico.className}`}>Zaber Ahmed</p>
+          <div className="flex flex-col items-start justify-center">
+            <p className={`text-lg text-extrabold ${pacifico.className}`}>
+              Zaber Ahmed
+            </p>
+            <p className={`text-sm ${pacifico.className}`}>Dhaka, Bangladesh</p>
+          </div>
         </div>
+        <div className="flex flex-col items-end justify-center">
+          <p className={`text-lg ${pacifico.className}`}>10 posts</p>
+          <p className={`text-sm ${pacifico.className}`}>
+            Software Engineer, Gamer, Cyclist
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <Button variant={"destructive"} size={"sm"}>
+          Logout
+          <LogOut size={"18px"} style={{ marginLeft: "8px" }} />
+        </Button>
         <Dialog>
           <DialogTrigger asChild>
-            <Button size={"md"}>
+            <Button size={"sm"}>
               Upload
               <Plus size={"18px"} style={{ marginLeft: "8px" }} />
             </Button>
@@ -31,10 +48,6 @@ const Profile = () => {
           <UploadContent />
         </Dialog>
       </div>
-      <Button variant={"destructive"} size={"sm"}>
-        Logout
-        <LogOut size={"18px"} style={{ marginLeft: "8px" }} />
-      </Button>
     </div>
   );
 };
