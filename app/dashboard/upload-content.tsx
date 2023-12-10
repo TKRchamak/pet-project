0d302316ed62b579beca0fe18879a01458b8f22e
Zaber Ahmed <zaber0305@gmail.com>
2023-12-10 14:30:58 +0600

    Add: resetting file after upload

--- app/dashboard/upload-content.tsx
+++ app/dashboard/upload-content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import {
   DialogContent,
   DialogDescription,
@@ -11,19 +11,26 @@ import {
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
+import { useToast } from "../components/ui/use-toast";
 
 const UploadContent = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploading, setUploading] = useState(false);
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const { toast } = useToast();
 
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
     setSelectedFile(file);
   };
 
-  const handleUpload = () => {
+  const handleUpload = (event: any) => {
     // Simulate an artificial delay using setTimeout
     if (!selectedFile) {
+      toast({
+        variant: "destructive",
+        title: "You have not selected any image!",
+      });
       return;
     }
 
@@ -32,8 +39,15 @@ const UploadContent = () => {
       console.log("Simulated upload complete");
       console.log("Selected file:", selectedFile);
       // Reset the file input and finish the artificial delay
+      if (imageInputRef.current) {
+        imageInputRef.current.value = "";
+      }
       setSelectedFile(null);
       setUploading(false);
+      toast({
+        title: "Successfully uploaded image!",
+        description: "You can upload another image.",
+      });
     }, 3000); // 3000 milliseconds (2 seconds) delay
   };
 
@@ -47,10 +61,9 @@ const UploadContent = () => {
       </DialogHeader>
       <div className="grid gap-4 py-4">
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="image" className="text-right">
-            Image
-          </Label>
+          <Label htmlFor="image">Image</Label>
           <Input
+            ref={imageInputRef}
             id="image"
             type="file"
             accept="image/*"
