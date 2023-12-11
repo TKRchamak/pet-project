4489a9e844d7d28c70789cf98aae784cff636f4a
Zaber Ahmed <zaber0305@gmail.com>
2023-12-11 14:32:23 +0600

    Fix: logo size number

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { buttonVariants } from "./ui/button";
 import Link from "next/link";
-import { SwitchCamera } from "lucide-react";
-import { lobster, pacifico } from "./fonts";
+import { Instagram } from "lucide-react";
+import { lobster } from "./fonts";
 
 const Logo = () => {
   return (
@@ -10,12 +10,12 @@ const Logo = () => {
       href={"/dashboard"}
       className={buttonVariants({
         className:
-          "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
+          "navLink hidden md:flex !mb-10 lg:hover:bg-transparent lg:!p-0 md:!px-2",
         variant: "ghost",
         size: "lg",
       })}
     >
-      <SwitchCamera className="h-8 w-8 shrink-0 lg:hidden" />
+      <Instagram className="h-8 w-8 shrink-0 lg:hidden" />
 
       <p
         className={`text-semibold text-3xl hidden lg:block ${lobster.className}`}
